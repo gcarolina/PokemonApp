5859a98370cab27543338335e45fd626b239cf75
gcarolina <karolinaganizoda@gmail.com>
2023-04-23 17:00:30 +0300

    Added UI for MainScreen

---
+++ PokemonApp/PokemonApp/MainScreen/MainTableCell/MainScreenCellViewModel.swift
@@ -0,0 +1,32 @@
+//
+//  MainScreenCellViewModel.swift
+//  PokemonApp
+//
+//  Created by Carolina on 23.04.23.
+//
+
+import UIKit
+
+final class MainScreenCellViewModel {
+    private let pokemonName: String
+    private let borderWidth: CGFloat = 4
+    private let cornerRadius = 30.0
+    
+    init(pokemonName: String) {
+        self.pokemonName = pokemonName
+    }
+    
+    func configureCell(_ cell: MainScreenTableViewCell) {
+        cell.pokemonName.text = pokemonName
+    }
+    
+    func setCornerRadius(view: UIView) {
+        view.layer.cornerRadius = CGFloat(cornerRadius)
+        view.layer.masksToBounds = true
+    }
+    
+    func setUpUIForBorder(view: UIView) {
+        view.layer.borderColor = UIColor(red: 10/255, green: 40/255, blue: 95/255, alpha: 1).cgColor
+        view.layer.borderWidth = borderWidth
+    }
+}

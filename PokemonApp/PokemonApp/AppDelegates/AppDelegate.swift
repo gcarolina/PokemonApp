c1420ca145317c4bec547f391d43fa8cc4f3ad35
gcarolina <karolinaganizoda@gmail.com>
2023-04-28 00:34:53 +0300

    Added Assets for UINavigationColor

--- PokemonApp/PokemonApp/AppDelegates/AppDelegate.swift
+++ PokemonApp/PokemonApp/AppDelegates/AppDelegate.swift
@@ -11,25 +11,23 @@ import UIKit
 class AppDelegate: UIResponder, UIApplicationDelegate {
 
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
-        UINavigationBar.appearance().barTintColor = UIColor.init(red: 10/255, green: 40/255, blue: 95/255, alpha: 1)
-        UINavigationBar.appearance().titleTextAttributes = [NSAttributedString.Key.foregroundColor : UIColor.init(red: 213/255, green: 161/255, blue: 0, alpha: 1)]
+        
+        if let navBarTintColor = UIColor(named: NavigationBarColor.navigationBarTintColor.rawValue) {
+            UINavigationBar.appearance().barTintColor = navBarTintColor
+        }
+        
+        if let navBarTitleTextColor = UIColor(named: NavigationBarColor.navigationBarTitleTextColor.rawValue) {
+            UINavigationBar.appearance().titleTextAttributes = [NSAttributedString.Key.foregroundColor : navBarTitleTextColor]
+        }
         return true
     }
 
     // MARK: UISceneSession Lifecycle
-
     func application(_ application: UIApplication, configurationForConnecting connectingSceneSession: UISceneSession, options: UIScene.ConnectionOptions) -> UISceneConfiguration {
-        // Called when a new scene session is being created.
-        // Use this method to select a configuration to create the new scene with.
         return UISceneConfiguration(name: "Default Configuration", sessionRole: connectingSceneSession.role)
     }
 
     func application(_ application: UIApplication, didDiscardSceneSessions sceneSessions: Set<UISceneSession>) {
-        // Called when the user discards a scene session.
-        // If any sessions were discarded while the application was not running, this will be called shortly after application:didFinishLaunchingWithOptions.
-        // Use this method to release any resources that were specific to the discarded scenes, as they will not return.
     }
-
-
 }
 
